0dddaf114b8e6fe809fc4f524f79e1d4839a3db5
Àlex Castelo <alexcastelomartinez@gmail.com>
2024-07-31 21:54:45 +0200

    clase 4: crear arquitectura MVC

--- app.js
+++ app.js
@@ -1,121 +1,19 @@
-const express = require("express");
-const crypto = require("node:crypto");
-const movieValidator = require("./schemas/movies");
-const cors = require("cors");
+import express, { json } from "express";
+import { corsMiddleware } from "./middlewares/cors.js";
 
 const PORT = process.env.PORT ?? 1234;
 const app = express();
-const movies = require("./movies.json");
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [
-        "http://localhost:8080",
-        "http://localhost:1234",
-        "https://movies.com",
-        "https://midu.dev",
-      ];
+import { moviesRouter } from "./routes/movies.js";
 
-      if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null, true);
-      }
-
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-  })
-);
+app.use(json());
+app.use(corsMiddleware());
 app.disable("x-powered-by");
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hola mundo" });
-});
-
-app.get("/movies", (req, res) => {
-  const { genre } = req?.query;
-
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    );
-
-    return filteredMovies.length
-      ? res.json(filteredMovies)
-      : res.status(404).json({ message: "No movies found with this genre" });
-  }
-
-  return res.json(movies);
-});
-
-app.get("/movies/:id", (req, res) => {
-  const { id } = req.params;
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie) return res.json(movie);
-
-  res.status(404).json({ message: "Movie not found" });
-});
-
-app.post("/movies", (req, res) => {
-  const result = movieValidator.validateMovie(req.body);
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const newMovie = {
-    id: crypto.randomUUID(),
-    ...result.data,
-  };
-
-  // Esto no sería REST, porque guardamos el estado de la aplicación en memoria
-  movies.push(newMovie);
-
-  res.status(201).json(newMovie);
-});
-
-app.patch("/movies/:id", (req, res) => {
-  const result = movieValidator.validatePartialMovie(req.body);
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data,
-  };
-
-  movies[movieIndex] = updateMovie;
-
-  return res.json(updateMovie);
-});
-
-app.delete("/movies/:id", (req, res) => {
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  movies.splice(movieIndex, 1);
-
-  return res.json({ message: "Movie deleted" });
-});
+app.use("/movies", moviesRouter);
 
 app.listen(PORT, () => {
   console.log(`server listening on port http://localhost:${PORT}`);
 });
+
+//https://youtu.be/ev3Yxva4wI4?si=sMHjLe0u71MDQbPT&t=2295
